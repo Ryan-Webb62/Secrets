3084584681b15b12f44368ba6134a40bd417b587
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-08-19 11:38:10 -0400

    Add ability to add secret and display

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
@@ -34,7 +35,9 @@ mongoose.connect('mongodb://localhost:27017/userDB',{
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -61,13 +64,23 @@ passport.use(new GoogleStrategy({
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/', function(req, res){
     res.render('home');
@@ -83,6 +96,16 @@ passport.authenticate('google', { failureRedirect: '/login' }),
       res.redirect('/secrets');
     });
 
+app.get('/auth/facebook',
+passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+passport.authenticate('facebook', { failureRedirect: '/login' }),
+function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 app.get('/login', function(req, res){
     res.render('login');
 });
@@ -91,8 +114,20 @@ app.get('/register', function(req, res){
 });
 
 app.get('/secrets', function(req, res){
+    User.find({'secret': {$ne: null}}, function(err, foundUsers){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render('secrets', {usersWithSecrets: foundUsers});
+            }
+        }
+    })
+});
+
+app.get('/submit', function(req, res){
     if (req.isAuthenticated()){
-        res.render('secrets');
+        res.render('submit');
     } else {
         res.redirect('/login');
     }
@@ -133,6 +168,23 @@ app.post('/login', function(req, res){
     })
 });
 
+app.post('/submit', function(req, res){
+    const submittedSecret = req.body.secret
+
+    console.log(req.user.id);
+    User.findById(req.user.id, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                res.redirect('/secrets');
+                });
+            }
+        }
+    });
+});    
 
 let port = process.env.PORT;
 if (port == null || port == '') {
